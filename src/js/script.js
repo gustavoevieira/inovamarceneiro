a9b949bb5b520b027bac0a665b0a973f8616a732
gustavoevieira <gustta.eudes@gmail.com>
2025-02-20 09:29:17 -0300

    redes sociais e popup

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,11 @@
 function toggleMenu() {
-    document.querySelector('.nav-list').classList.toggle('active');
-  }
+  document.querySelector('.nav-list').classList.toggle('active');
+}
 
-  function closeMenu() {
-    document.querySelector('.nav-list').classList.remove('active');
-  }
+function closeMenu() {
+  document.querySelector('.nav-list').classList.remove('active');
+}
+
+function fecharPopup() {
+  document.getElementById("modal").classList.add("hidden");
+}
